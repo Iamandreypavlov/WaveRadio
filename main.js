c4238de483c6d87d5361498ed0fed258cbd567c3
Andrew Pavlov <andrey.pavlov13@mail.ru>
2024-12-18 19:28:16 +0300

    no changes

--- main.js
+++ main.js
@@ -2,8 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("burger").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("open")
   })
-})
-
-
-
+});
 
